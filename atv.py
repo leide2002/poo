8a6e0c0a6b034e9d7d841b902325c2b835faa7aa
unknown <laidy34x@gmail.com>
2025-05-14 16:25:37 -0300

    teste do poo

--- atv.py
+++ atv.py
@@ -1,6 +1,32 @@
-from biblioteca import Pessoa
+from biblioteca import *
+
+
+'''
 aluno01 = Pessoa("Leide",22,67)
 print(aluno01.nome)
 aluno01.apresentar()
 aluno01.comer()
 aluno01.dormindo()
+'''
+bicho=Gato("Dilma","branca")
+bicho.comer()
+bicho.miar()
+
+fazenda= Cachorro("lula","preto")
+fazenda.comer()
+fazenda.latir()
+
+ani=Coelho("d","d")
+ani.comer()
+ani.grunir()
+
+mu=Vaca("gaga","rosa")
+mu.comer()
+mu.mujir()
+
+valor=10.00
+cinema= Ingresso(valor)
+cinema.imprimirValor()
+
+cinemavip= Vip(valor)
+cinemavip=imprimirValor()
--- biblioteca.py
+++ biblioteca.py
@@ -19,3 +19,65 @@ class Pessoa():
             print(f"{self.nome} está dormindo e não pode comer!")
         else:
             print(f"{self.nome} está comendo. Hmmm!")
+
+class Animal():
+            def __init__(self,nome,cor):
+                self.nome=nome
+                self.cor=cor
+            def comer(self):
+                print(f"o {self.nome} foi comer")
+class Gato(Animal):
+            def __init__(self,nome,cor):
+                super().__init__(nome,cor)
+
+            def miar (self):
+                print(f"o {self.nome} esta miando...")
+class Cachorro(Animal):
+     def __init__(self,nome,cor):
+         super().__init__(nome,cor)
+
+     def latir(self):
+         print(f"{self.nome}, esta latindo auau")
+
+class Coelho (Animal):
+    def __init__(self,nome,cor):
+         super().__init__(nome,cor)
+
+    def grunir(self):
+        print(f"{self.nome}, esta grunindo.")
+class Vaca (Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome,cor)
+    def mujir(self):
+        print(f"{self.nome} esta mujindo muuuu.")
+
+class Ingresso ():
+    def __init__(self, valor): #metodo construtor que vai receber o valor
+        self.valor=valor
+    def imprimirValor(self):
+        print(f"o valor do ingresso e, {self.valor} ")
+
+class Vip (Ingresso):
+    def __init__(self, valor):
+        super().__init__(valor*1.5)
+    def impimeValor(self):
+        print(f"O valor do ingresso é R$ {self.valor:.2}")
+class Forma():
+    def __init__(self):
+        self.area=0
+        self.perimetro=0
+
+class Triangulo(Forma):
+    def __init__(self, altura, base):
+        self.base=base
+        self.altura=altura
+
+    def calcularArea(self):
+
+        print(f"o valor da area e {self.area}")
+
+        super().calcularArea(self.perimetro*3)
+    def calcularPerimetro(self):
+
+
+
